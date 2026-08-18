b6a1df266308e2ba5cb0594c98c341e8a5695af0
mellamoeterno <richmyself8@gmail.com>
2026-08-17 23:04:03 -0300

    additions

---
+++ app/teaching/callback.js
@@ -0,0 +1,39 @@
+/* 
+
+First of all things, you dont need to read what ive written here, this is just so i can keep very good track of what i need to say, otherwise
+i wont be able to teach all of what i want. Or just do wathever, thats on you.
+
+
+i want to talk about one of the most important basic React/javascript things but with also some different syntax ways to use callback
+so you can get ahead on understanding more of callback
+
+
+******
+Before all these are things im assuming you already know:
+
+
+about arrow functions overall () => {} 
+    
+if statements
+
+functions
+
+variables/objects/let/const & Invoking them/adding properties 
+
+How objects in javascript work
+
+the basics of how props work
+
+    
+console.log
+
++ operator
+
+    
+    
+unfortunately, it would be a bit too much information and it would get confusing, 
+so you may want to learn about these pre hand, or this lesson will be 
+not so fruitfull.
+******
+
+*/
