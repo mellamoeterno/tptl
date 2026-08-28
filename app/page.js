3a5f006e72aab999b20a09e05b6047270588db40
mellamoeterno <richmyself8@gmail.com>
2026-08-28 15:04:33 -0300

    fixingHEADmissmatches

---
+++ app/page.js
@@ -0,0 +1,24 @@
+'use client'
+import { Button } from "../app/Zcomponents/Button.js"
+
+/**
+ * Challenge: Add an onClick handler to the Button
+ * component instance (the one on this page). Then
+ * make whatever changes you need for it to actually
+ * work.
+ * 
+ * Clicking the button should log "Logging in..." 
+ * to the console.
+ */
+
+export default function Home() {
+  const clicked = () => {console.log('Logging in')}
+  return (
+    <main>
+      <Button onClick={clicked}>
+        Log in with Google
+      </Button>
+    </main>
+  )
+}
+
